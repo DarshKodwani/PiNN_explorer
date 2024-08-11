aaca55e1019e7706e77fdc9b0a806d8f4331961a
Darsh Kodwani <ddkcdk@yahoo.co.uk>
2024-08-11 18:45:12 +0100

    adding fTS with additional environment variables for base directory

--- firstTimeSetup.sh
+++ firstTimeSetup.sh
@@ -4,10 +4,17 @@ set -xeuf -o pipefail
 
 #git submodule update --init --recursive (This is needed if you have submodules and want them to stay up to date)
 
+# Get the absolute path of the repository root
+BASE_DIR=$(pwd)
+
+# Store the BASE_DIR in an environment file
+echo "BASE_DIR=$BASE_DIR" > .env
+
 rm -rf venv
 
 python3 -m venv venv
 source venv/bin/activate
+
 
 pip install --upgrade pip
 pip install -r requirements.txt
